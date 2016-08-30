6cad0eaea40b796e6d6f6b500a344456dfcb1167
Christian Rohleder <sm0ove@me.com>
2016-08-30 21:45:34 +0200

    Build Prozess konfiguriert.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,57 +1,64 @@
-var webpack = require('webpack');
-var path = require('path');
-var loaders = require('./webpack.loaders');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const loaders = require('./webpack.loaders');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
 
 // local css modules
 loaders.push({
-	test: /[\/\\]src[\/\\].*\.css$/,
-	loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]')
+  test: /[\/\\]src[\/\\].*\.css$/,
+  loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'),
 });
 // global css files
 loaders.push({
-	test: /[\/\\](node_modules|global)[\/\\].*\.css$/,
-	loader: ExtractTextPlugin.extract('style', 'css')
+  test: /[\/\\](node_modules|global)[\/\\].*\.css$/,
+  loader: ExtractTextPlugin.extract('style', 'css'),
+});
+// local css modules
+loaders.push({
+  test: /[\/\\]src[\/\\].*\.less$/,
+  loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader'),
 });
 
 module.exports = {
-	entry: [
-		'./src/index.jsx'
-	],
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: '[chunkhash].js'
-	},
-	resolve: {
-		extensions: ['', '.js', '.jsx']
-	},
-	module: {
-		loaders
-	},
-	plugins: [
-		new WebpackCleanupPlugin(),
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"production"'
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false,
-				screw_ie8: true,
-				drop_console: true,
-				drop_debugger: true
-			}
-		}),
-		new webpack.optimize.OccurenceOrderPlugin(),
-		new ExtractTextPlugin('[contenthash].css', {
-			allChunks: true
-		}),
-		new HtmlWebpackPlugin({
-			template: './src/template.html',
-			title: 'Webpack App'
-		})
-	]
+  entry: [
+    './src/browser.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: '[chunkhash].js',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  module: {
+    loaders,
+  },
+  plugins: [
+    /* new WebpackCleanupPlugin({
+      exclude: ['assets/', 'fonts/'],
+    }),*/
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        drop_console: true,
+        drop_debugger: true,
+      },
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new ExtractTextPlugin('[contenthash].css', {
+      allChunks: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: './server/template.html',
+      title: 'Webpack App',
+    }),
+  ],
 };
